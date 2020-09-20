097a35e974fe5eb439194159153b20d8df916fb4
laksamana.akbar <laksamana.guntur@mekar.id>
2020-09-20 23:28:01 +0700

    Second Commit,
    - Add talkback feature on pics and button

--- app/src/main/java/co/id/viewandviews/MainActivity.kt
+++ app/src/main/java/co/id/viewandviews/MainActivity.kt
@@ -2,6 +2,7 @@ package co.id.viewandviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,7 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.title = "Google Pixel"
 
+        val pauseText = resources.getString(R.string.pause)
+        pause_button.contentDescription = pauseText
     }
 }
